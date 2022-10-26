04c441d6766bd0ca02ed67f238be3129df3df048
Phyu Phyu Thin <phyuthin2004@gmail.com>
2022-10-26 11:30:25 +0630

    socket.io

---
+++ index.js
@@ -0,0 +1,14 @@
+let express = require('express')
+
+// app setup
+let app = express()
+
+//server setup
+let server = app.listen(4000, ()=>{
+    console.log('Project is running at localhost 4000.')
+})
+
+// route setup
+app.get('/', (res, req)=>{
+    req.sendFile(__dirname + '/public/index.html');
+})
